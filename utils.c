c24c6d668abbcb759b719ca127adbb1124240ebd
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-04-20 05:38:54 +0000

    removal

--- utils.c
+++ utils.c
@@ -12,22 +12,6 @@ void print_prompt(void)
 }
 
 /**
- * handle_eof - handles end of file condition
- * @line: pointer to getline buffer
- * @interactive: checks if shell is in interactive mode
- *
- * Return: nothing
- */
-void handle_eof(char *line, int interactive)
-{
-	if (interactive)
-		printf("\n"); /* print newline on Ctrl + D */
-
-	free(line); /* free getline buffer before exiting */
-	exit(0); /* exit shell successfully */
-}
-
-/**
  * clean_input - removes newline from the input string
  * @line: input string read by getline
  *
